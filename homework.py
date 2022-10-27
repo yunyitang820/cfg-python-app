71d06e6dd36d06e693d415a661c9a3391780046b
Yunyi <winifred.tang.0820@gmail.com>
2022-10-27 18:56:11 +0100

    Initial commit

---
+++ homework.py
@@ -0,0 +1,260 @@
+# Code First Girls: Python Session 1
+# Question 1
+# I am building some very high quality chairs and need exactly four nails for each chair.
+# I've written a program to calculate how many nails I need to buy to build these chairs.
+chairs = 15
+nails = 4
+total_nails = chairs * nails
+message = 'I need to buy {} nails'.format(total_nails)
+print('You need to buy {} nails'.format(message))
+# When I run the program it tells me that I need to buy 15151515 nails.
+# This seems like a lot of nails.
+# Is my program calculating the total number of nails correctly? What is the problem? How do I fix it?
+
+# Question 2
+# I'm trying to run this program, but I get an error.
+# What is the error telling me is wrong? How do I fix the program?
+my_name = 'Penelope'
+my_age = 29
+message = 'My name is {} and I am {} years old'.format(my_name, my_age)
+print(message)
+
+# Question 3
+# I have a lot of boxes of eggs in my fridge and I want to calculate how many omelettes I can make.
+# Write a program to calculate this.
+# Assume that a box of eggs contains six eggs and I need four eggs for each omelette,
+# but I should be able to easily change these values if I want.
+# The output should say something like "You can make 9 omelettes with 6 boxes of eggs".
+eggs_per_box = int(input('How many eggs per box did you buy? '))
+number_eggs_boxes = int(input('How many boxes of eggs do you have? '))
+number_eggs = number_eggs_boxes * eggs_per_box
+each_omelette = 4
+number_omelette = int(number_eggs/each_omelette)
+number_omelette_msg = 'You can make {} omelettes with {} boxes of eggs'.format(number_omelette, number_eggs_boxes)
+print(number_omelette_msg)
+
+# Code First Girls: Python Session 2
+# Question 1
+# Explain what this program does
+for number in range(100):
+    output = 'o' * number
+print(output)
+
+# Question 2
+# Your boss really likes calculating VAT on their purchases. It is their favourite hobby.
+# They've written this code to calculate VAT and need your help to fix it.
+def calculate_vat(amount):
+    return amount * 1.2
+total = calculate_vat(100)
+print(total)
+# When your boss runs the program they get the following output: None
+# Your boss expects the program to output the value 120. What is wrong? How do you fix it?
+
+# Code First Girls: Python Session 3
+# Question 1
+# Create a program that tells you whether or not you need an umbrella when you leave the house.
+# The program should:
+# 1. Ask you if it is raining using input()
+# 2. If the input is 'y', it should output 'Take an umbrella'
+# 3. If the input is 'n', it should output 'You don't need an umbrella'
+raining = input('Is it raining? y/n ')
+if raining == 'y':
+    print('Take an umbrella.')
+else:
+    print('You don\'t need an umbrella')
+
+
+# Question 2
+# I'm on holiday and want to hire a boat. The boat hire costs £20 + a refundable £5 deposit. I've
+# written a program to check that I can afford the cost, but something doesn't seem right.
+# Have a look at my program and work out what I've done wrong
+my_money = int(input('How much money do you have? '))
+boat_cost = 20 + 5
+if my_money < boat_cost:
+    print('You can afford the boat hire')
+else:
+    print('You cannot afford the board hire')
+
+# Question 3
+# Your friend works for an antique bookshop that sells books between 1800 and 1950 and wants to
+# quickly categorise books by the century and decade that they were written.
+# Write a program that takes a year (e.g. 1872) and outputs the century and decade
+# (e.g. "Nineteenth Century, Seventies")
+def decade_to_text(decade):
+    if decade >= 90:
+        return 'Nineties'
+    elif decade >= 80:
+        return 'Eighties'
+    elif decade >= 70:
+        return 'Seventies'
+    elif decade >= 60:
+        return 'Sixties'
+    elif decade >= 50:
+        return 'Fifties'
+    elif decade >= 40:
+        return 'Forties'
+    elif decade >= 30:
+        return 'Thirties'
+    elif decade >= 20:
+        return 'Twenties'
+    elif decade >= 10:
+        return 'Tens'
+    else:
+        return 'Hundreds'
+written_year = int(input('Which year this book was wrote? From 1800 to 1950 '))
+decade = written_year % 100
+if written_year < 1800 or written_year > 1950:  # further refine on generosity
+    print('Sorry, this book is not sold at this antique bookshop.')
+elif written_year // 100 == 19:
+    century = 'Twentieth Century'
+    print(century + ', ' + decade_to_text(decade))
+elif written_year // 100 == 18:
+    century = 'Nineteenth Century'
+    print(century + ', ' + decade_to_text(decade))
+
+# written_year = int(input('Which year this book was wrote? From 1800 to 1950 '))
+# if written_year >= 1900 and written_year <= 1950:
+#     century = 'Twentieth Century'
+#     decade = written_year - 1900
+# if written_year >= 1800 and written_year <1900:
+#     century = 'Nineteenth Century'
+#     decade = written_year - 1800
+# def decade_to_text(decade):
+#     if decade >= 90:
+#         return 'Nineties'
+#     elif decade >= 80:
+#         return 'Eighties'
+#     elif decade >= 70:
+#         return 'Seventies'
+#     elif decade >= 60:
+#         return 'Sixties'
+#     elif decade >= 50:
+#         return 'Fifties'
+#     elif decade >= 40:
+#         return 'Forties'
+#     elif decade >= 30:
+#         return 'Thirties'
+#     elif decade >= 20:
+#         return 'Twenties'
+#     elif decade >= 10:
+#         return 'Tens'
+#     else:
+#         return 'Hundreds'
+# if written_year >= 1800 and written_year <= 1950:
+#     print(century + ', ' + decade_to_text(decade))
+# else:
+#     print('Sorry, this book is not sold at this antique bookshop.')
+
+# if written_year == 1950:
+#     print('Twentieth Century, Fifties')
+# elif written_year < 1950 and written_year >= 1940:
+#     print('Twentieth Century, Forties')
+# elif written_year < 1940 and written_year >= 1930:
+#     print('Twentieth Century, Thirties')
+# elif written_year < 1930 and written_year >= 1920:
+#     print('Twentieth Century, Twenties')
+# elif written_year < 1920 and written_year >= 1910:
+#     print('Twentieth Century, Tenth')
+# elif written_year < 1910 and written_year >= 1900:
+#     print('Twentieth Century, Hundreds')
+# elif written_year < 1950 and written_year >= 1940:
+#     print('Twentieth Century, Forties')
+# elif written_year < 1940 and written_year >= 1930:
+#     print('Twentieth Century, Thirties')
+# elif written_year < 1930 and written_year >= 1920:
+#     print('Twentieth Century, Twenties')
+# elif written_year < 1920 and written_year >= 1910:
+#     print('Twentieth Century, Tens')
+# elif written_year < 1910 and written_year >= 1900:
+#     print('Twentieth Century, Hundreds')
+# elif written_year < 1900 and written_year >= 1890:
+#     print('Nineteenth Century, Nineties')
+# elif written_year < 1890 and written_year >= 1880:
+#     print('Nineteenth Century, Eighties')
+# elif written_year < 1880 and written_year >= 1870:
+#     print('Nineteenth Century, Seventies')
+# elif written_year < 1870 and written_year >= 1860:
+#     print('Nineteenth Century, Sixties')
+# elif written_year < 1860 and written_year >= 1850:
+#     print('Nineteenth Century, Fifties')
+# elif written_year < 1850 and written_year >= 1840:
+#     print('Nineteenth Century, Forties')
+# elif written_year < 1840 and written_year >= 1830:
+#     print('Nineteenth Century, Thirties')
+# elif written_year < 1830 and written_year >= 1820:
+#     print('Nineteenth Century, Twenties')
+# elif written_year < 1820 and written_year >= 1810:
+#     print('Nineteenth Century, Tens')
+# elif written_year < 1810 and written_year >= 1800:
+#     print('Nineteenth Century, Hundreds')
+
+# Code First Girls: Python Session 3
+# Question 1
+# I have a list of things I need to buy from my supermarket of choice.
+shopping_list = [
+    "oranges",
+    "cat food",
+    "sponge cake",
+    "long-grain rice",
+    "cheese board",
+    ]
+print(shopping_list[0])
+# I want to know what the first thing I need to buy is. However, when I run the program it shows me a
+# different answer to what I was expecting?
+# What is the mistake? How do I fix it?
+
+# Question 2
+# I'm setting up my own market stall to sell chocolates. I need a basic till to check the prices of
+# different chocolates that I sell.
+# I've started the program and included the chocolates and their prices. Finish the program by asking
+# the user to input an item and then output its price.
+chocolates = {
+    'white': 1.50,
+    'milk': 1.20,
+    'dark': 1.80,
+    'vegan': 2.00,
+    }
+request = input('What chocolates do you want? white/milk/dark/vegan ')
+print('The price of this chocolates is ' + str(chocolates[request]) + '.')
+
+# Question 3
+# Write a program that simulates a lottery. The program should have a list of seven numbers that
+# represent a lottery ticket. It should then generate seven random numbers. After comparing the two
+# sets of numbers, the program should output a prize based on the number of matches:
+# ● £20 for three matching numbers
+# ● £40 for four matching numbers
+# ● £100 for five matching numbers
+# ● £10000 for six matching numbers
+# ● £1000000 for seven matching numbers
+
+import random
+lottery_range = range(50)
+
+def draw():
+    win_ticket = [random.randrange(1, 50), random.randrange(1, 50), random.randrange(1, 50), random.randrange(1, 50),
+                  random.randrange(1, 50), random.randrange(1, 50), random.randrange(1, 50)]
+    return win_ticket
+print("Lucky Numbers:", draw())
+
+your_ticket = [1, 3, 5, 7, 9, 11, 13]
+win_ticket = draw()
+
+def compare():
+    matches = []
+    for num in your_ticket:
+        if num in win_ticket:
+            matches.append(num)
+    if len(matches) == 3:
+        print('You WIN £20 for three matching numbers')
+    elif len(matches) == 4:
+        print('You WIN £40 for four matching numbers')
+    elif len(matches) == 5:
+        print('You WIN £100 for five matching numbers')
+    elif len(matches) == 6:
+        print('You WIN £10,000 for six matching numbers')
+    elif len(matches) == 7:
+        print('You WIN £1,000,000 for seven matching numbers!')
+    else:
+        print('You win nothing. Try again!')
+
+compare()
---
+++ main.py
@@ -0,0 +1,365 @@
+# Code First Girls: Python Session 1
+print('Hello world')
+print(50)
+print(10/3 == 10%3)
+
+#example of variables
+#strings 'hellow'
+#interger 100
+#double 5.5
+
+firstname = 'Yunyi'
+lastname = 'Tang'
+print(firstname)
+print(lastname)
+fullname = firstname + ' ' + lastname
+print(fullname)
+
+salary = 100
+expenses = 70
+savings = salary - expenses
+#one way
+print('Salary : ' + str(salary))
+#another way
+print(f'Salary : {salary}')
+
+# exercise: We want to store customer details for kate glover's shop
+# Full name
+# Address
+# Age
+
+fullname = input('What is your full name? ')
+address = input('What is your address? ')
+age = input('What is your age? ')
+agein5years = int(age) + 5
+
+print(f'Welcome to our shop : {fullname}')
+print(f'In 5 years time, you would be this years old : {agein5years}')
+print('Thank for your address. Just to confirm. It is : ' + address)
+
+# assignment
+number1 = float(input('Please give me the first number '))
+number2 = float(input('Please give me the second number '))
+sum = number1 + number2
+print(f'The summary of your numbers is {sum}')
+
+# assignment 2
+days = 31
+hours = 24
+total_hours = days * hours
+
+msg = "There are {} in {} days".format(total_hours, days)
+print(msg)
+
+# Code First Girls: Python Session 2
+# 1. User Input
+# 2. Importing modules
+# 3. For Loops
+# 4. While Loops
+# 5. Functions
+total = 0
+print("*** This statement is OUTSIDE THE LOOP")
+print("Before the loop executes, our TOTAL is equal to  = ", total, '\n')
+print("--------------------------------------------------------")
+for number in range(3):  # remember --> 0, 1, 2
+    print("This is loop execution for digit: " + str(number) + " inside the loop \n")
+    print("Updating total... (+ 1) \n")
+    total = total + 1  # every time the loop executes, we add 1 to the total
+print("--------------------------------------------------------")
+print("***This statementWe is OUTSIDE the loop now")
+print("The final TOTAL value is: " + str(total))
+
+store_capacity = 5
+while store_capacity > 0:
+   print('Please come in. Spaces available: ' + str(store_capacity))
+   store_capacity =  store_capacity - 1
+print("\nPlease wait for someone to exit the store.")
+
+# Code First Girls: Python Session 3
+# 1. Comparison Operators
+# 2. Logical Operators
+# 3. If Statements
+
+# Exercise 3.1: You have a budget of £10 and want to write a program to decide which burger restaurant to go to.
+# 1. Input the price of a burger using input()
+# 2. Check whether the price is less than or equal (<=) 10.00
+# 3. Print the result in the format below
+# Burger is within budget: True
+# Hint: remember to convert the input from a string to a decimal with float()
+price = input('What is the price? ')
+within_budget = float(price) <= 10.00
+print('Burger is within budget: {}'.format(within_budget))
+
+# Exercise 3.2: Add code to your burger program to input whether the restaurant has a vegetarian option
+# The output should say whether the cost is within budget AND has a vegetarian option
+# Restaurant meets criteria: True
+price = input('What is the price? ')
+veg_option = input('Do they have vegeterian options? y/n ')
+within_budget = float(price) <= 10.00
+has_veg_option = veg_option == 'y'
+print('Burger is within budget: {}'.format(within_budget and has_veg_option))
+
+name = input("What is your name? ")
+is_admin = name == 'admin'
+password = input("What is your password? ")
+password_correct = password == 'loveu'
+if is_admin and password_correct:
+    print('Welcome')
+if not is_admin or not password_correct:
+    print('No access')
+
+# Exercise 3.3: Rewrite the output of your burger program to use if statements
+# If it is a good choice it should be: This restaurant is a great choice!
+# If it is not a good choice it should be: Probably not a good idea
+price = input('What is the price? ')
+within_budget = float(price) <= 10.00
+veg_option = input('Do they have vegeterian options? y/n ')
+has_veg_option = veg_option == 'y'
+if within_budget and veg_option:
+    print('This restaurant is a great choice!')
+if not within_budget or not has_veg_option:
+    print('Probably not a good idea')
+
+password = input('password: ')
+if password == 'loveu':
+    print('Success!')
+else:
+    print('Failure!')
+
+name = input("What is your name? ")
+is_admin = name == 'admin'
+password = input("What is your password? ")
+password_correct = password == 'loveu'
+if is_admin and password_correct:
+    print('Welcome')
+else:
+    print('No access')
+
+# Exercise 3.4: Now that you've nished your burger, you want to pay for your food.
+# Let's write a program to calculate your meal and apply a discount if applicable.
+# If your total meal costs more than £20 and you have a discount, the price will be reduced by 10%.
+# The program should print "Discount applied" or "No discount" depending on whether the discount criteria was met.
+meal_price = float(input('How much did the meal cost? '))
+discount_choice = input('Do you have a discount? y/n ')
+applicable_price = float(meal_price) > 20
+discount_applicable = discount_choice == 'y'
+applicable = applicable_price and discount_applicable
+if applicable:
+    meal_price = meal_price * 0.9
+    print('Discount applied')
+else:
+    print('No discount')
+print('Total cost: {}'.format(meal_price))
+
+dog_size = int(input('How big is the dog? '))
+if dog_size > 75:
+    print('That is a big dog')
+elif dog_size < 25:
+    print('That is a small dog')
+else:
+    print('That is an average dog')
+
+# Exercise 3.5: You're cooking a pizza and need to check that the oven is at the right temperature.
+# Write a program to:
+# Ask the user to input the temperature
+# Prints "The oven is too hot" if the temperature is over 200
+# Prints "The oven is too cold" if the temperature is under 150
+# Prints "The oven is at the perfect temperature" if the temperature is 180
+# Prints "The temperature is close enough" for any other temperature
+temperature = int(input('What is the temperature? '))
+if temperature > 200:
+    print('The oven is too hot.')
+elif temperature < 150:
+    print('The oven is too cold.')
+elif temperature == 180:
+    print('The oven is at the perfect temperature.')
+else:
+    print('The temperature is close enough.')
+
+# Python has a built-in library for random data
+import random
+random_integer = random.randint(1, 100)
+print(random_integer)
+
+# The randint() function generates a random number between two values
+# This program uses randint() to simulate dice with any number of sides
+import random
+sides = int(input('How many sides does the die have? '))
+random_integer = random.randint(1, sides)
+print('You rolled a {}'.format(random_integer))
+
+# To practice if statements choose one of the following exercises in your student guide:
+# Exercise 3.6: Flip a coin
+# Exercise 3.7: Rock, Scissors, Paper
+# Exercise 3.8: Roulette
+
+# Code First Girls: Python Session 4
+# 1. Lists
+# 2. Dictionaries
+
+# Exercise
+name = input('What is your name? ')
+if name == 'Tara' or name == 'Tara ':
+    print('Welcome Admin')
+else:
+    print('Welcome {}'.format(name)) # or print(f'Welcome {name}')
+
+# Exercise
+movies = ['pride and prejudice', 'arrival', 'twilingt', 'smile', 'interstellar']
+print(f'Before appending, I have these movies: {movies}')
+# methods: a piece of code that is called by name that is associated with an object
+movies.append('Just Mercy')
+print(f'After appending, I have these movies: {movies}')
+# function: a piece of code that is called by a name
+# used to find the length or size of a list
+print(len(movies))
+# remove from a list, .pop remove the very last item of a list
+movies.pop()
+print(f'After popping, we now have {movies}')
+
+# Exercise
+# We have a shop called 'Yunyi shop'. We want to add new customers to our shop
+# but we need their names
+# if a new customer is already an existing customer,
+# we want to tell the customer they are already registered
+yunyi_customers = ['Samuel']
+new_customers = input('What is your name? ')
+if new_customers in yunyi_customers:
+    print('Oh no, you are already a customer!')
+else:
+    yunyi_customers.append(new_customers)
+print('Now our customers are ' + str(yunyi_customers))
+
+# Exercise
+movies = ['pride and prejudice', 'arrival', 'twilingt', 'smile', 'interstellar']
+first_movie = movies[0]
+last_movie = movies[-1]
+# three ways to find the last element:
+# 1. count
+# 2. length - 1
+# 3. -1
+print(first_movie)
+print(last_movie)
+movies.pop(0)
+print(movies)
+
+# Exercise
+# create a list of your top 5 celebrities
+# ask the user for a celebrity name.
+# if the name the user gives is in your list,
+# say 'You rock!' else say 'Try again'
+celeb = ['a', 'b', 'c', 'd', 'e']
+new_celeb = input('What is your top celebrities? ')
+if new_celeb in celeb:
+    print('You rock!')
+else:
+    print('Try again')
+
+# Exercise
+ages = [5, 12, 4, 9, 17]
+under18 = []
+over18 = []
+for age in ages:
+    if age < 18:
+        under18.append(age)
+    else:
+        over18.append(age)
+print(under18)
+print(over18)
+
+for age in ages:
+    print(age)
+
+# pre-built function
+length_of_ages = len(ages)
+max_ages = max(ages)
+min_ages = min(ages)
+sorted_ages = sorted(ages, reverse=True)
+
+print(length_of_ages)
+print(max_ages)
+print(min_ages)
+print(sorted_ages)
+
+# Exercise
+lunchCostsThisWeek = [5, 3, 6, 2, 8, 3, 9]
+sum = 0
+for lunchCost in lunchCostsThisWeek:
+    sum = sum + lunchCost
+print(sum) # or print(sum(lunchCostsThisWeek))
+
+# Exercise
+person = {}
+person['name'] = input('What is your name? ')
+person['address'] = input('What is your address? ')
+print(person)
+
+# Assignment
+# You are the owner of Yunyi shop. You have a customer in front of you
+# and you want to store that customer's details (e.g name, age, phone) in an
+# object/dictionary. If the age of the customer is > 60, assign 'Senior Citizen' to
+# the 'citizen' attribute in the object/dictionary.
+# If not, assign 'Junior Citizen'
+# Create a program that does this
+yunyi_customers = {}
+yunyi_customers['name'] = input('What is your name? ')
+yunyi_customers['phone'] = input('What is your phone number? ')
+yunyi_customers['age'] = int(input('What is your age? '))
+if yunyi_customers['age'] > 60:
+    yunyi_customers['citizen'] = 'Senior Citizen'
+else:
+    yunyi_customers['citizen'] = 'Junior Citizen'
+print(yunyi_customers)
+
+# nesting objects
+place = {
+    'name': 'The Anchor',
+    'post_code': 'E14 6HY',
+    'street_number': '54',
+    'location': {
+        'longitude': 127,
+        'latitude': 63,
+        }
+}
+
+# IndexError: list index out of range
+cheeses = [
+    'brie',
+    'cheddar',
+    'wensleydale',
+    'edam',
+]
+print(cheeses[4])
+
+# SyntaxError: '[' was never closed
+trees = [
+    {'leaf_colour': 'green', 'height': 2120},
+    {'leaf_colour': 'green', 'height': 2300}]
+new_tree = {
+    'leaf_colour': 'green',
+    'height': 1020
+}
+trees.append(new_tree)
+print(trees)
+
+# Random
+import random
+colours = ['red', 'green', 'blue']
+chosen_colour = random.choice(colours)
+print(chosen_colour)
+
+# Code First Girls: Python Session 5
+# 1. Files
+# 2. Pip package manager
+# 3. APIs
+
+# Writing to a file
+new_item = input('Enter a to-do item: ')
+with open('todo.txt', 'r') as todo_file:
+    todo = todo_file.read()
+    todo = todo + new_item + '\n'
+with open('todo.txt', 'w+') as todo_file:
+    todo_file.write(todo)
+
+
+
